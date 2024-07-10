c85fc4dadad6274c3899ff9134bec8397125e676
keelann95 <sephen.mwaniki@student.moringaschool.com>
2024-07-10 13:27:12 +0300

    modified  index.js

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// code your solution here
+function superbowlWin(record) {
+    let winningYear;
+    // Find the first record where the result is 'W'
+    const winningRecord = record.find(entry => entry.result === 'W');
+    
+    // If such a record exists, return its year; otherwise, return undefined
+    if (winningRecord) {
+      winningYear = winningRecord.year;
+    }
+  
+    return winningYear;
+  }
+  
